c80f49752145873e218858efedac6ae42ba073e9
KimHyeonBong <vongright@gmail.com>
2024-12-10 11:06:37 +0900

    feat: byte[] 기반 송수신을 위한 변환메서드 추가 및 테스트

--- src/main/java/org/gosqo/tcpsocket/HexConverter.java
+++ src/main/java/org/gosqo/tcpsocket/HexConverter.java
@@ -6,6 +6,61 @@ import java.nio.charset.StandardCharsets;
 public class HexConverter {
     public static final Charset BASE_CHARSET = StandardCharsets.ISO_8859_1;
 
+    static String bytesToString(byte[] bytes, Charset charset) {
+        return new String(bytes, charset);
+    }
+
+    static String bytesToHexExpression(byte[] bytes) {
+        StringBuilder builder = new StringBuilder();
+
+        for (byte b : bytes) {
+            builder.append("%02X ".formatted(b));
+        }
+
+        return builder.substring(0, builder.length() - 1);
+    }
+
+    static byte[] hexExpressionToBytes(String hexExp) throws IllegalArgumentException {
+
+        if (hexExp == null || hexExp.isEmpty()) {
+            throw new IllegalArgumentException("Input string cannot be null or empty");
+        }
+
+        String trimmed = hexExp.trim();
+        String[] split = trimmed.contains(" ") ? trimmed.split(" ") : splitEach2(trimmed);
+        byte[] bytes = new byte[split.length];
+
+        for (int i = 0; i < split.length; i++) {
+            String each = split[i];
+
+            if (each.length() != 2) {
+                throw new IllegalArgumentException("cannot pair in 2 digit hex");
+            }
+
+            bytes[i] = Byte.parseByte(each, 16); // NumberFormatException
+        }
+
+        return bytes;
+    }
+
+    static String[] splitEach2(String s) {
+        int length = s.length();
+
+        if (length % 2 != 0) {
+            throw new IllegalArgumentException("cannot pair in 2 characters");
+        }
+
+        String[] ret = new String[length / 2];
+
+        for (int i = 0; i < length; i += 2) {
+            String substring = s.substring(i, i + 2);
+
+            ret[i / 2] = substring;
+        }
+
+        return ret;
+    }
+
     static String hexStringToUTF_8Encoded(String hex) throws IllegalArgumentException {
         int length = hex.length();
 
@@ -21,7 +76,7 @@ public class HexConverter {
 
             if (digit1 < 0 || digit2 < 0) {
                 throw new IllegalArgumentException("One or more digits is not in the Hexadecimal range."
-                + "\n\tOtherwise, please check if 'Enter in Hex' is checked.");
+                        + "\n\tOtherwise, please check if 'Enter in Hex' is checked.");
             }
 
             data[i / 2] = (byte) (
---
+++ src/test/java/org/gosqo/tcpsocket/HexConverterTest.java
@@ -0,0 +1,50 @@
+package org.gosqo.tcpsocket;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class HexConverterTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "68656c6c6f"
+            , "6g656c6c6f"
+            , "68 65 6c 6c 6f"
+            , "68 65 6c 6c 6f"
+            , "68 65 6c 6c 6f 0a"
+            , "68 65 6c 6c 6f 0d 0a"
+    })
+    void hexExpressionToBytes(String param) {
+        byte[] bytes = HexConverter.hexExpressionToBytes(param);
+
+        System.out.println(Arrays.toString(bytes));
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideByteArrays")
+    void bytesToHexExpression(byte[] param) {
+        String hexExp = HexConverter.bytesToHexExpression(param);
+
+        System.out.println(hexExp);
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideByteArrays")
+    void bytesToString(byte[] param) {
+        String s = HexConverter.bytesToString(param, HexConverter.BASE_CHARSET);
+
+        System.out.println(s);
+    }
+
+    private static Stream<byte[]> provideByteArrays() {
+        return Stream.of(
+                new byte[]{(byte) 0x68, (byte) 0x65, (byte) 0x6c, (byte) 0x6c, (byte) 0x6f}
+                , new byte[]{(byte) 0x68, (byte) 0x65, (byte) 0x6c, (byte) 0x6c, (byte) 0x6f, (byte) 0x0d}
+                , new byte[]{(byte) 0x68, (byte) 0x65, (byte) 0x6c, (byte) 0x6c, (byte) 0x6f, (byte) 0x0d, (byte) 0x0a}
+        );
+    }
+}
