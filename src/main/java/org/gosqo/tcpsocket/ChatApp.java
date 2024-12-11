674ed38594f07341e4365f4f93d037f5fd1b4a9c
KimHyeonBong <vongright@gmail.com>
2024-12-11 16:47:43 +0900

    update: design: font RobotoMono 로 변경

--- src/main/java/org/gosqo/tcpsocket/ChatApp.java
+++ src/main/java/org/gosqo/tcpsocket/ChatApp.java
@@ -1,7 +1,11 @@
 package org.gosqo.tcpsocket;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.Objects;
 
 public class ChatApp extends Application {
 
@@ -13,8 +17,15 @@ public class ChatApp extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+        Scene scene = mainView.createScene();
+        URL mainStylesheet = Objects.requireNonNull(
+                getClass().getResource("main.css")
+        );
+
+        scene.getStylesheets().add(mainStylesheet.toExternalForm());
+
         stage.setTitle("TCP socket");
-        stage.setScene(mainView.createScene());
+        stage.setScene(scene);
         stage.setOnCloseRequest(event -> mainView.stageClose());
         stage.show();
     }
