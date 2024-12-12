79e6e589d0b3a14ed781552adf0e03af1b46f951
KimHyeonBong <vongright@gmail.com>
2024-12-12 19:11:29 +0900

    design: app window title 변경, 버전 정보 추가

--- src/main/java/org/gosqo/tcpsocket/ChatApp.java
+++ src/main/java/org/gosqo/tcpsocket/ChatApp.java
@@ -9,6 +9,7 @@ import java.util.Objects;
 
 public class ChatApp extends Application {
 
+    public static final String APP_VERSION = "1.1.0";
     private static final MainView mainView = new MainView();
 
     public static void main(String[] args) {
@@ -17,6 +18,7 @@ public class ChatApp extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+        String title = "TCP socket by gosqo " + APP_VERSION;
         Scene scene = mainView.createScene();
         URL mainStylesheet = Objects.requireNonNull(
                 getClass().getResource("main.css")
@@ -24,7 +26,7 @@ public class ChatApp extends Application {
 
         scene.getStylesheets().add(mainStylesheet.toExternalForm());
 
-        stage.setTitle("TCP socket");
+        stage.setTitle(title);
         stage.setScene(scene);
         stage.setOnCloseRequest(event -> mainView.stageClose());
         stage.show();
